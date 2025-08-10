142857181dd331a92e89c26170c10664dae8ef0d
Abdul Musabbir <126709118+abdul-musabbir@users.noreply.github.com>
2025-08-10 16:36:40 +0600

    fix the type errors

--- resources/js/components/layout/nav-user.tsx
+++ resources/js/components/layout/nav-user.tsx
@@ -25,7 +25,7 @@ export function NavUser() {
                     <DropdownMenuTrigger asChild>
                         <SidebarMenuButton size="lg" className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
                             <Avatar className="h-8 w-8 rounded-lg">
-                                <AvatarImage src={user?.avatar} alt={user.name} />
+                                <AvatarImage src={''} alt={user.name} />
                                 <AvatarFallback className="rounded-lg">SN</AvatarFallback>
                             </Avatar>
                             <div className="grid flex-1 text-left text-sm leading-tight">
@@ -44,8 +44,14 @@ export function NavUser() {
                         <DropdownMenuLabel className="p-0 font-normal">
                             <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                                 <Avatar className="h-8 w-8 rounded-lg">
-                                    <AvatarImage src={user.avatar} alt={user.name} />
-                                    <AvatarFallback className="rounded-lg">SN</AvatarFallback>
+                                    <AvatarImage src={''} alt={user.name} />
+                                    <AvatarFallback className="rounded-lg">
+                                        {user.name
+                                            .split(' ')
+                                            .slice(0, 1)
+                                            .map((word) => word[0])
+                                            .join('')}
+                                    </AvatarFallback>
                                 </Avatar>
                                 <div className="grid flex-1 text-left text-sm leading-tight">
                                     <span className="truncate font-semibold">{user.name}</span>
